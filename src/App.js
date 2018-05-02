42b69a3c73e7ad3a3f4f27ca20fbf3213173d755
Jazlyn Ang <angjazlyn@hotmail.com>
2018-05-02 15:07:11 +0800

    extracta cookie component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import React, { Component } from 'react';
 import cookie from './cookie.png';
 import './App.css';
 
-const box = <Box label="Cookies"/>;
+const box = <Box label="Chocolate Chip"/>;
 
+// can define a component this way
 class App extends Component {
   render() {
     return box;
   }
 }
 
+// or define a component this way
 function Box(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={cookie} className="App-logo" alt="logo" />
-        <h1 className="App-title">{props.label}</h1>
+        <Cookie label={props.label}/>
       </header>
     </div>
   );
+}
+
+class Cookie extends Component {
+  render() {
+    return (
+      <div>
+        <img src={cookie} className="App-logo" alt="logo" />
+        <h1 className="App-title">{this.props.label} Cookies</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
