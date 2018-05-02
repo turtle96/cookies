e17a0fa1210aabc169daaec8d450f75f6974721f
Jazlyn Ang <angjazlyn@hotmail.com>
2018-05-02 15:33:36 +0800

    add a clock

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// All React components must act like pure functions with respect to their props.
+
 import React, { Component } from 'react';
 import cookie from './cookie.png';
 import './App.css';
@@ -6,7 +8,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <Clock/>
         <Box label="Chocolate Chip"/>
         <Box label="Lemon Barley"/>
       </div>
@@ -16,8 +19,9 @@ class App extends Component {
 
 // or define a component this way
 function Box(props) {
+  // className used to specify a CSS class, check App.css
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <Cookie label={props.label}/>
       </header>
@@ -31,6 +35,25 @@ class Cookie extends Component {
       <div>
         <img src={cookie} className="App-logo" alt="logo" />
         <h1 className="App-title">{this.props.label} Cookies</h1>
+      </div>
+    );
+  }
+}
+
+// using component allows use of additional features such as local state and lifecycle hooks
+class Clock extends React.Component {
+  constructor(props) {
+    // call the base constructor with props
+    super(props);
+
+    // assigns the initial this.state
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="App-intro">It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
