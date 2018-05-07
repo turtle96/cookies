dccc69868e7e0b45d85bb072e7d687aae3b9844a
Jazlyn Ang <angjazlyn@hotmail.com>
2018-05-07 18:38:14 +0800

    at last a test test that app renders clock

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Clock from "./Clock";
+import {mount} from "enzyme";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('app should render a clock', () => {
+  const wrapper = mount(<App />);
+  const clock = <Clock/>;
+  expect(wrapper.contains(clock)).toEqual(true);
 });
