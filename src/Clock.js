98608928ec92f9de3993ef7152e7c821fbc6ee95
turtle96 <angjazlyn@hotmail.com>
2020-07-20 21:43:52 +0800

    reformat file, set clock to not show milliseconds

--- src/Clock.js
+++ src/Clock.js
@@ -2,47 +2,47 @@
 import React from "react";
 
 class Clock extends React.Component {
-  constructor(props) {
-    // call the base constructor with props
-    super(props);
+    constructor(props) {
+        // call the base constructor with props
+        super(props);
 
-    // assigns the initial this.state
-    this.state = {date: new Date(Date.now())};
-  }
+        // assigns the initial this.state
+        this.state = {date: new Date(Date.now())};
+    }
 
-  ///// lifecycle hooks setup
+    ///// lifecycle hooks setup
 
-  // runs after the component output has been rendered to the DOM
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
+    // runs after the component output has been rendered to the DOM
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
 
-  // called when a component is being removed from the DOM
-  componentWillUnmount() {
-    // clears the timer
-    clearInterval(this.timerID);
-  }
+    // called when a component is being removed from the DOM
+    componentWillUnmount() {
+        // clears the timer
+        clearInterval(this.timerID);
+    }
 
-  tick() {
-    // updates the state when called
-    // on setState() call
-    // React knows the state has changed, and calls the render() method again
-    this.setState({
-      date: new Date(Date.now())
-    });
+    tick() {
+        // updates the state when called
+        // on setState() call
+        // React knows the state has changed, and calls the render() method again
+        this.setState({
+            date: new Date(Date.now())
+        });
 
-    // do not modify this.state directly i.e. this.state.comment = 'Hello';
-    // State Updates May Be Asynchronous
-  }
+        // do not modify this.state directly i.e. this.state.comment = 'Hello';
+        // State Updates May Be Asynchronous
+    }
 
-  render() {
-    return (
-      <h3>It is {this.state.date.toLocaleTimeString()}.</h3>
-    );
-  }
+    render() {
+        return (
+            <h3>It is {this.state.date.toLocaleTimeString().replace(/(.*)\D\d+/, '$1')}.</h3>
+        );
+    }
 }
 
 export default Clock;
